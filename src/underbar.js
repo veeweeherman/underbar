bcdf6d0e06b86d52fe2becfb0144ea5b893a433b
veeweeherman <vy.hoang.cu@gmail.com>
2015-06-16 01:10:21 -0700

    defaults, only tests1-2 passing

--- src/underbar.js
+++ src/underbar.js
@@ -353,6 +353,12 @@ _.uniq = function(array, isSorted, iterator) {
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(argObj) {
+      _.each(argObj, function(val, key) {
+        obj[key] = val; 
+      });
+    });
+    return obj;
   };
 
 
